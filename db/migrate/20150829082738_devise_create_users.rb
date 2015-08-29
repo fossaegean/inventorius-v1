5e1748c845494886c07dac2a947e856714f6a2ea
Christos Sotirelis <sotirelisc@gmail.com>
2015-08-29 08:34:04 +0000

    Fix User migration.

--- db/migrate/20150829082738_devise_create_users.rb
+++ db/migrate/20150829082738_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string avatar
+      t.string :avatar
 
       ## Recoverable
       t.string   :reset_password_token
